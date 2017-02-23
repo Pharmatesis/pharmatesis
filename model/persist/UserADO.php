af63c5df4af0b0f9ff99c9689acacd538b4d87b9
dev <dev@Pharmatesis-PC>
2017-02-23 20:22:53 +0100

    Structure commit 3

---
+++ model/persist/UserADO.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of UserADO
+ *
+ * @author dev
+ */
+class UserADO {
+    //put your code here
+}
